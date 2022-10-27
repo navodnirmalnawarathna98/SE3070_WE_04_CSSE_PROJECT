b37ebb060e53f6da00da8ab27b3b43e19c122e06
Navod Nirmal Nawarathna <novanawarathna@gmail.com>
2022-10-27 19:04:50 +0530

    update

--- SE3070_WE_04_CSSE_WebApp/front-end/src/App.js
+++ SE3070_WE_04_CSSE_WebApp/front-end/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./Pages/Home-Page/HomePage";
 
+import LoginPage from "./Pages/Login-Page/LoginPage";
+
 function App() {
   return (
     <Router>
@@ -9,6 +11,9 @@ function App() {
 
         {/* Home Page */}
         <Route path='/' element={<HomePage />} />
+
+        {/* Login Page */}
+        <Route path='/login' element={<LoginPage />} />
 
       </Routes>
     </Router>
