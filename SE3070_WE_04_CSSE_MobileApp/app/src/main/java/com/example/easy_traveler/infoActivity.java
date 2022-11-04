9df20bdc9369495e52570fea8ec1df548d1b3348
Navod Nirmal Nawarathna <novanawarathna@gmail.com>
2022-11-04 16:48:31 +0530

    update

---
+++ SE3070_WE_04_CSSE_MobileApp/app/src/main/java/com/example/easy_traveler/infoActivity.java
@@ -0,0 +1,43 @@
+package com.example.easy_traveler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class infoActivity extends AppCompatActivity {
+
+    Button show;
+    singleTonExample singletonexample;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        show = findViewById(R.id.show);
+        singletonexample = singletonexample.getInstance();
+        singletonexample.init(getApplicationContext());
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                singletonexample.AlertDialog(infoActivity.this);
+            }
+        });
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            if (bundle.getString("some") != null){
+                Toast.makeText(getApplicationContext(),
+                        "data:" + bundle.getString("some"),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+    }
+}
